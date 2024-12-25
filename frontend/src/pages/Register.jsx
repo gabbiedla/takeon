643d45c3b776999c21ff7d7232b15b2150233b37
Fulcrum <sokatanocw@gmail.com>
2024-12-25 15:00:51 -0800

    added password visibility

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,19 +1,30 @@
 import { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import {
+  Form,
+  Button,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 import Loader from '../components/Loader';
 import { useRegisterMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
 import { toast } from 'react-toastify';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // For the eye icon
 
 const Register = () => {
   const [name, setName] = useState('');
   const [username, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false); // State to toggle password visibility
+
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false); // State for confirm password visibility
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -90,8 +101,39 @@ const Register = () => {
         </Form.Group>
         <Form.Group controlId="password" className="my-3">
           <Form.Label>Password</Form.Label>
+          <InputGroup>
+            <FormControl
+              type={passwordVisible ? 'text' : 'password'} // Toggle between text and password
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <InputGroup.Text
+              onClick={() => setPasswordVisible(!passwordVisible)}
+            >
+              {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+            </InputGroup.Text>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group controlId="confirmPassword" className="my-3">
+          <Form.Label>Confirm Password</Form.Label>
+          <InputGroup>
+            <FormControl
+              type={confirmPasswordVisible ? 'text' : 'password'} // Toggle between text and password
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <InputGroup.Text
+              onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
+            >
+              {confirmPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+            </InputGroup.Text>
+          </InputGroup>
+        </Form.Group>
+        {/* <Form.Group controlId="password" className="my-3">
+          <Form.Label>Password</Form.Label>
           <Form.Control
-            // name="password"
             type="password"
             placeholder="Enter password"
             value={password}
@@ -101,13 +143,12 @@ const Register = () => {
         <Form.Group controlId="confirmPassword" className="my-3">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
-            // name="password"
             type="password"
             placeholder="Confirm password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
-        </Form.Group>
+        </Form.Group> */}
 
         <Button
           type="submit"
